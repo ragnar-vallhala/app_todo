53a390764df7954c306f7a991b4a01b2a998c0c6
ASHUTOSH VISHWAKARMA <117195143+ragnar-vallhala@users.noreply.github.com>
2023-08-09 00:01:33 +0530

    8 august 2023

--- app/src/main/java/com/ashu/todo/MainActivity.java
+++ app/src/main/java/com/ashu/todo/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton add_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        * Object instantiation
+        * */
+        add_button = findViewById(R.id.add_button);
+
+
+
+        /*
+        * Add button Navigates to the add_task_activity
+        * */
+
     }
+
+
+
 }
